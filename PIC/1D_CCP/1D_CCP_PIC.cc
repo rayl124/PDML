3184c53d0f55d4958ee46973c8e14b4923364362
rayl124 <rl49@rice.edu>
2020-05-18 19:44:08 -0500

    Updated 1D CCP

--- PIC/1D_CCP/1D_CCP_PIC.cc
+++ PIC/1D_CCP/1D_CCP_PIC.cc
@@ -22,26 +22,38 @@ weights[1] = hx;
 // solves laplace(phi) = -rho/epsilon0 = f
 void jacobi_Update(double *phi,
 		double *RHS,
+		int *elec_range,
+		double phi_left,
+		double phi_right,
 		double dx,
 		int nn,
 		const int jacobi_max_iter,
 		const double tol,
 		const double n0,
 		const double phi0,
-		const double q,
+		const double e,
 		const double epsilon0,
 		const double k,
 		const double Te) {
 
   double *phi_new = new double[nn];
   double *RHS0 = new double[nn];
-  copy_n(phi, nn, phi_new);
-  copy_n(RHS, nn, RHS0);
-
   
   int jacobi_iter = 0;
   double residual;
   
+  // Add dirichlet boundaries
+  for (int i = elec_range[0]; i <= elec_range[1]; ++i) {
+    phi[node] = phi_left;
+  }
+
+  for (int i = elec_range[2]; i <= elec_range[3]; ++i) {
+    phi[node] = phi_right;
+  }
+
+  copy_n(phi, nn, phi_new);
+  copy_n(RHS, nn, RHS0);
+
   while (jacobi_iter <= jacobi_max_iter) {
     ++jacobi_iter;
     residual = 0.0;
@@ -52,16 +64,14 @@ void jacobi_Update(double *phi,
 
     // Update RHS term to include electron terms
     for (int i = 0; i < nn; ++i) {
-      RHS[i] = RHS0[i] - n0*exp((phi[i]-phi0)/(Te));
+      RHS[i] = RHS0[i] - n0*exp(q*(phi[i]-phi0)/(k*Te));
       RHS[i] = -q*RHS[i]/epsilon0;
     }
 
     // Run Jacobi update for interior nodes
-    for (int i = 1; i < nn - 1; ++i) {
-      phi_new[i] = 0.5*pow(dx,2)*(phi[i-1] + phi[i+1] - RHS[i]);
+    for (int i = elec_range[1] + 1; i < elec_range[2]; ++i) {
+      phi_new[i] = 0.5*(phi[i-1] + phi[i+1] - pow(dx,2.0)*RHS[i]);
     }
-
-    // Run Jacobi update for Neumann boundaries
 
     // Calculate the residual
     for (int i = 0; i < nn; ++i) {
@@ -92,7 +102,7 @@ int main(void) {
 
   // Constantsa
   const double epsilon0 = 8.854e-12; // Permittivity of free space
-  const double q = 1.602e-19; // Elementary charge [C]
+  const double e = 1.602e-19; // Elementary charge [C]
   const double k = 1.381e-23; // Boltzman constant [J/K]
   const double AMU = 1.661e-27; // Atomic Mass Unit [kg]
   const double M = 32.0*AMU; // Ion mass of O2 [kg]
@@ -100,15 +110,15 @@ int main(void) {
   // Input settings
   double n0 = 1.0e12; // Electron density [#/m^3]
   double phi0 = 0.0; // Reference potential
-  double Te = 1.0; // Electron temp [eV]
-  double Ti = 0.1; // Ion temp [eV]
+  double Te = 3000; // Electron temp [K], figure this out later
+  double Ti = 300; // Ion temp [K]
   double v_drift = 7.0e3; // Ion injection velocity [m/s]
   double phi_p = -5.0; // Plate potential [V]
 
   // DOUBLE CHECK THESE FORMULAS !!!!!!!///
   // Plasma parameters
-  double lambdaD = sqrt(epsilon0*Te/(q*n0));
-  double vth = sqrt(2.0 * q * Ti/M);
+  //double lambdaD = sqrt(epsilon0*Te/(q*n0));
+  double vth = sqrt(2.0 * k * Ti/M);
 
   // Problem discretization
   int nn = 191; // # of x1 nodes
@@ -131,22 +141,24 @@ int main(void) {
   double f_lf = 271.2e3; // Hz
   double V_hf = 110.0; // V
   double V_lf = 281.9; // V
+  double phi_left;
+  double phi_right = 0.0;
 
   // Particle info
   int np_real = round(n0*v_drift*Lx2*dt); // Number of real particles entering
-  double sp_wt = np_real/np_insert; // Real/Macroparticle
-  double mp_q = 1; // Macroparticle charge
   int max_part = 4e6; // max_particles if none leave during time history
-
+  
   // Particle data
-  double *part_x = new double[max_part]; // Even indices = x1 values
-  double *part_v = new double[max_part]; // Odd indices = x2 values
-  double *rho = new double[nn];
+  double *part_q = new double[max_part]; // Macroparticle charge
+  double *part_spwt = np_real/np_insert; // Particle weight
+  double *part_x = new double[max_part]; // Position
+  double *part_v = new double[max_part]; // Velocity
 
   // Various helper variabes
   // Charge density
   double *weights = new double[2];
   double *node_index = new double[max_part];
+  double *rho = new double[nn]; // Density at each node
 
   // Electric potential
   double *RHS = new double[nn];
@@ -166,25 +178,31 @@ int main(void) {
   
   // Set up phi boundaries and initial values
   double *phi = new double[nn];
+  double t;
 
   for (int i = 0; i < nn; ++i) {
-    phi[i] = 0.0;
+     phi[i] = 0.0;
   }
  
   // Output files
-  ofstream DataFile("Results/ESFieldData.txt");
+  ofstream FieldFile("Results/ESFieldData.txt");
   ofstream ParticleFile("Results/ParticleInfo.txt");
   ofstream NumFile("Results/NumParticles.txt");
 
-  DataFile << "Iteration / Ion Density / Electric Potential /";
-  DataFile << "Electric Field" << endl;
+  FieldFile << "Iteration / Ion Density / Electric Potential /";
+  FieldFile << "Electric Field" << endl;
 
-  ParticleFile << "Iteration / x1 / x2 / v1 / v2";
+  ParticleFile << "Iteration / x / v / spwt / q";
   ParticleFile << endl;
 
   NumFile << "Iteration / Number of Macroparticles" << endl;
   // Main Loop
-  for (int iter = 0; iter < ts; ++iter) {     
+  for (int iter = 0; iter < ts; ++iter) {
+    // Electrode Potential
+    t = iter*dt;
+    phi_left = V_hf*sin(2*M_PI*f_hf*t) +  
+	    V_lf*sin(2*M_PI*f_lf*t);
+
     // Reset variables
     for (int i = 0; i< nn; ++i) {
       rho[i] = 0.0;
@@ -194,18 +212,24 @@ int main(void) {
     
     cout << "Iter: " << iter << endl;
     cout << "Computing ion charge density..." << endl;
+
+    /////////////////////////////////////////////////
+
     //Compute ion charge density 
     
-    ///////////////////////////
+    /////////////////////////////////////////////////
+    
+    // Get charge from particles
     for (int p = 0; p < np; ++p) {
       get_Weights(x_part[p], weights, &node_index[p], dx);
       
-      rho[node_index[p]] += weights[0];
-      rho[node_index[p+1]] += weights[1];
+      rho[node_index[p]] += part_spwt[p]*part_q[p]*weights[0];
+      rho[node_index[p]+1] += part_spwt[p]*part_q[p]*weights[1];
     }
 
+    // Accont for volume
     for (int i = 0; i < nn; ++i) {
-      rho[i] *= sp_wt*mp_q/(dx*dx);
+      rho[i] /= dx*dx;
 
       // If on the boundary, half the volume.
       if (i == 0 || i == nn-1) {
@@ -216,22 +240,27 @@ int main(void) {
     }
     
     cout << "Computing electric potential..." << endl;
+    //////////////////////////////////////////////////
+
     // Compute electric potential
     
-    ////////////////////////////////
-    jacobi_Update(phi, RHS, box_range, dh, dh, nx1, nx2, jacobi_max_iter, tol,
-		    n0, phi0, q, epsilon0, k, Te);
+    /////////////////////////////////////////////////
+    jacobi_Update(phi, RHS, elec_range, phi_left, phi_right,
+		    dx, nn, jacobi_max_iter, tol, 
+		    n0, phi0, e, epsilon0, k, Te);
 
     cout << "Computing electric field..." << endl;
+    /////////////////////////////////////////////////
+
     // Compute electric field
     
-    ////////////////////////////////////////
+    //////////////////////////////////////////////////
     for (int i = 0; i < nn; ++i) {
       // Left
       if (i == 0) {
 	E_field[index_ij] = -(phi[1] - phi[0])/dx;
       } // Right
-      else if (i == nx1-1) {
+      else if (i == nn-1) {
 	E_field[index_ij] = -(phi[i] -  phi[i-1])/dx;
       }
       else {
@@ -240,17 +269,18 @@ int main(void) {
     }
 
     cout << "Moving Particles..." << endl;
+    //////////////////////////////////////////////////
+
     // Move particles
     
-    ///////////////////////////////////////////
+    //////////////////////////////////////////////////
     // Uses same weights as charge density
     for (int p = 0; p < np; ++p) {      
 
       part_E = E_field[node_index[p]]*weights[0] +
 	          E_field[node_index[p] + 1]*weights[1];
 
-      part_v[p] = part_v[p] +(q/M)*part_E*dt;
-
+      part_v[p] = part_v[p] + (part_q[p]/M)*part_E*dt;
       part_x[p] = part_x[p] + part_v[p]*dt;
  
       /*
@@ -300,21 +330,23 @@ int main(void) {
     // Output info
     if ((iter+1)%25 == 0) {
       for (int i = 0; i < nn; ++i) {
-        DataFile << iter << " " << rho[i] << " " << phi[i] << " " << E_field[2*i] << " ";
-	DataFile << E_field[2*i + 1] << endl;
+        FieldFile << iter << " " << rho[i] << " " << phi[i] << " " << E_field[i];
+	FieldFile << endl;
       }
       for (int i = 0; i < np; ++i) {
-        ParticleFile << iter << " " << x_part[2*i] << " " << x_part[2*i+1] << " " ;
-	ParticleFile << v_part[2*i] << " " << v_part[2*i+1] << endl;
+        ParticleFile << iter << " " << part_x[i] << " " << part_v[i] << " ";
+	ParticleFile << part_spwt[i] << " " << part_q[i] << endl;
       }
 
       NumFile << iter << " " << np << endl;
     }
   }
 
-  delete(box_range);
+  delete(elec_range);
   delete(part_x);
   delete(part_v);
+  delete(part_q);
+  delete(part_spwt);
   delete(rho);
   delete(weights);
   delete(node_index);
